c63a5cd4347439a3e02df497f1bb4ecb576956f2
stefanus efan <stefanusefan@gmail.com>
2022-08-10 11:26:30 +0700

    Portfolio luisconnecting

---
+++ contact.php
@@ -0,0 +1,114 @@
+<?php 
+
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Cek apakah ada data yang belum diisi
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClassk = 'errordiv';
+        }else{
+            // Pengaturan penerima email dan subjek email
+            $toEmail = 'info@luisconnecting.com'; // Ganti dengan alamat email yang Anda inginkan
+            $emailSubject = 'Pesan website dari '.$name;
+            $htmlContent = '<h2> Pesan Dari Website</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Subject</h4><p>'.$subject.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+            
+            // Mengatur Content-Type header untuk mengirim email dalam bentuk HTML
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Header tambahan
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Pesan anda sudah terkirim !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Pesan anda gagal dikirim';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Silahkan melengkapi semua data';
+        $msgClass = 'errordiv';
+    }
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta name="keywords" content="jasa penagihan piutang, Outsourcing tenaga penagihan, jasa pengamanan, bodyguard" />
+<meta name="description" content="jasa penagihan piutang perusahaan terhadap perusahaan lain, jasa penagihan piutang perusahaan terhadap perorangan,Outsourcing tenaga penagihan dan jasa penagihan hutang perorangan terhadap orang lain" />
+<link rel="shortcut icon" href="images/favicon.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>luis connecting</title>
+<link href="css/Style.css" rel="stylesheet" type="text/css" />
+ <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script><script type="text/javascript">var search_input= $('.search input[type=text]');var search_input_size = 120; var search_large_size = 180; var padding = 7; var shrinked = "";$(document).ready(function(){search_input.click(function(){shrink();}).focus(function(){shrink();});search_input.blur(function(){ if(shrinked=="YES")normal();});$('.button a').hover(function(){if(shrinked=="YES")normal();}); });function shrink(){if(search_input_size < search_large_size ){$('.button a').each(function(){$(this).animate({'padding-left': padding+'px','padding-right': padding+'px'},'fast');});search_input.animate({'width': search_large_size+'px'},'fast'); shrinked="YES";}return false;}function normal(){search_input.animate({'width':search_input_size+'px'},'fast'); $('.button a').animate({'padding-left':'10px','padding-right':'10px'},'fast');shrinked="";search_input.blur();return false;}</script>
+ 
+</head>
+
+<body>
+<div  id="header"></div>
+<div id="navbar">
+  <ul id="menu_wrap" class="Vista_Black"><li class="button"><a href="index.php">Home</a></li><li class="button"><a href="profil.php">Profil</a></li><li class="button"><a href="contact.php">Hubungi Kami</a></li><li class="button"><a href="#">Informasi</a></li> <li class="search"><a href="#"></a><input type="text" placeHolder="Search"  /></li></ul>
+</div>
+<div id="contak-kami">
+<div id="form-kontak"><img src="images/Kontak-kami.jpg" width="400" height="186" />
+<div class="mainContent">
+  Gunakan formulir dibawah ini untuk menghubungi kami
+  <div class="contactFrm">
+        <?php if(!empty($statusMsg)){ ?>
+        <p class="statusMsg <?php echo !empty($msgClass)?$msgClass:''; ?>"><?php echo $statusMsg; ?></p>
+        <?php } ?>
+        <form action="" method="post">
+            <h4>Name</h4>
+            <input type="text" name="name" placeholder="Nama Anda" required="">
+            <h4>Email </h4>
+            <input type="email" name="email" placeholder="email@example.com" required="">
+            <h4>Subject</h4>
+            <input type="text" name="subject" placeholder="Tulis subject" required="">
+            <h4>Message</h4>
+            <textarea name="message" placeholder="Tulis pesan Anda disini" required=""> </textarea>
+            <input type="submit" name="submit" value="Kirim">
+            <div class="clear"> </div>
+        </form>
+    </div>
+</div>
+</div>
+<div id="contak-kami1">
+<h3>Kator Pusat</h3>
+   <p>
+   	 Jl. Mitiara Taman Palem Blok A18 No.33<br />
+	 Tlp : 0813 8146 9828<br />
+       WA :0812 9032 6699 <br />
+Email : <a href="mailto:info@luisconnecting.com">info@luisconnecting.com</a><br />
+website :<a href="http://www.luisconnecting.com" target="_blank">www.luisconnecting.com</a><br />
+    </p>
+       <p align="center"><img src="images/luisconnecting.png" width="200" height="200" /></p>
+    <h3>Media</h3>
+    <a href="https://www.facebook.com/" target="_blank"><img src="images/facebook.png" width="50" height="50" /></a><a href="https://twitter.com/login?lang=id" target="_blank"><img src="images/twitter logo.png" width="50" height="50" /></a><a href="https://www.youtube.com/" target="_blank"><img src="images/yt.png" width="80" height="50" /></a><br />
+    <br />
+    <br />
+
+
+</div>
+</div>
+<div id="sahdow1"></div>
+    <div id="footer">
+    
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta name="keywords" content="jasa penagihan piutang, Outsourcing tenaga penagihan, jasa pengamanan, bodyguard" />
+<meta name="description" content="jasa penagihan piutang perusahaan terhadap perusahaan lain, jasa penagihan piutang perusahaan terhadap perorangan,Outsourcing tenaga penagihan dan jasa penagihan hutang perorangan terhadap orang lain" />
+<link rel="shortcut icon" href="images/favicon.ico">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>luis connecting</title>
+<link href="css/Style.css" rel="stylesheet" type="text/css" />
+ <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script><script type="text/javascript">var search_input= $('.search input[type=text]');var search_input_size = 120; var search_large_size = 180; var padding = 7; var shrinked = "";$(document).ready(function(){search_input.click(function(){shrink();}).focus(function(){shrink();});search_input.blur(function(){ if(shrinked=="YES")normal();});$('.button a').hover(function(){if(shrinked=="YES")normal();}); });function shrink(){if(search_input_size < search_large_size ){$('.button a').each(function(){$(this).animate({'padding-left': padding+'px','padding-right': padding+'px'},'fast');});search_input.animate({'width': search_large_size+'px'},'fast'); shrinked="YES";}return false;}function normal(){search_input.animate({'width':search_input_size+'px'},'fast'); $('.button a').animate({'padding-left':'10px','padding-right':'10px'},'fast');shrinked="";search_input.blur();return false;}</script>
+ 
+ <!-- Insert to your webpage before the </head> -->
+    <script src="sliderengine/jquery.js"></script>
+    <script src="sliderengine/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="sliderengine/amazingslider-1.css">
+    <style type="text/css">
+    body {
+	background-color: #F0F0F0;
+}
+    </style>
+    <script src="sliderengine/initslider-1.js"></script>
+    <!-- End of head section HTML codes -->
+ 
+</head>
+
+<body>
+<div  id="header"></div>
+<div id="navbar">
+  <ul id="menu_wrap" class="Vista_Black"><li class="button"><a href="index.php">Home</a></li><li class="button"><a href="profil.php">Profil</a></li><li class="button"><a href="contact.php">Hubungi Kami</a></li><li class="button"><a href="#">Informasi</a></li> <li class="search"><a href="#"></a><input type="text" placeHolder="Search"  /></li></ul>
+</div>
+<div id="galery">
+    	 <!-- Insert to your webpage where you want to display the slider -->
+    <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:990px;margin:0px auto 0px;">
+        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+            <ul class="amazingslider-slides" style="display:none;">
+                <li><img src="images/galery1.jpg" />
+                </li>
+                <li><img src="images/1.jpg" />
+                </li><!-- -->
+                <li><img src="images/2.jpg" />
+                </li>
+            </ul>
+        <!-- --></div>
+    </div>
+    <!-- End of body section HTML codes -->
+    </div>
+<div id="shadow1"></div>
+<div id="isi"><P>
+  <img src="images/logo.jpg" width="400" height="138" /></P>
+<P><b>Luis Connecting</b> merupakan perusahaan sewasta yang melayani jasa penagihan piutang perusahaan terhadap perusahaan lain,
+jasa penagihan piutang perusahaan terhadap perorangan, dan jasa penagihan hutang perorangan terhadap orang lain. </P>
+<P>Selain melayani penagihan,<b> Luis Connecting</b> juga melayani jasa pengaman yang terdiri dari pengaman  perusahaan,
+pengaman aset, pengaman pribadi (Bodyguard) dan pengaman event.
+</P>
+<P>Saat ini kami mememiliki 50 orang staff yang sudah berpengalaman dan terlatih secara profesional, dan kami juga dapat menyediakan staff lepas yang mana fungsinya akan di sesuaikan dengan kondisi di lapangan. Dalam menjalankan tugas, staff kami selalu mengedepankan norma hukum, dan setiap efek hukum yang terjadi adalah tanggung jawab kami sesuai dengan surat kontrak yang ada.</P>
+
+<p>Sampai saat ini kami sudah memiliki banyak client baik itu dari perusahaan maupun perorangan dan mendapatkan hasil yang memuaskan buat mereka.</p>
+
+</div>
+<div id="isi2">
+<h3>KAMI MENYEDIAKAN JASA YANG MELIPUTI :</h3>
+<h4>A. Penagihan Utang</h4>
+<H5>1. Jasa penagihan Piutang perusahaan terhadap perusahaan lain<br />
+2. Jasa Penagihan Piutang Perusahaan terhadap perorangan<br />
+3. Jasa Penagihan Hutang Perusahaan Terhadap Perorangan<br />
+4. Jasa Penagihan Hutang Perorangan Terhadap Orang Lain<br />
+</H5>
+<h4>B. Jasa Pengamanan
+<h5>1.Jasa Pengamanan Perusahaan<br />
+2.Jasa Pengamanan Aset<br />
+3.Jasa Pengamanan Pribadi(Bodyguard)<br />
+4. Jasa Pengamanan Event</h5>
+<h4>SISTEM PEMBAYARAN</h4>
+<p>Untuk Jasa Penagihan Hutang sistem pembayaran yang kami gunakan adalah sistem “Success Fee” berupa Persentase Dana yang dapat kami tagih. Succes Fee dibayar setelah tugas yang kami jalankan berhasil. Persentase Succes Fee dapat dinegosiasikan tergantung tingkat kesulitan yang dihadapi. Sedangkan untuk Jasa pengaman dapat dinegosiasikan sesuai dengan tingkat kesulitan yang akan dihadapi.</p>
+
+</div>
+<div id="Isi3">
+<div id="isi-kontak"><img src="images/contak-luisconnecting.jpg" width="400" height="372" />
+</div>
+<div id="isi-kontak2"><img src="images/luisconnecting.png" width="200" height="200" /></div>
+</div>
+<div id="sahdow1"></div>
+    <div id="footer">
+    
+</div>
+<!-- start popup -->
+    <div id="close">
+        <div class="container-popup">
+            <form action="#" method="post" class="popup-form">
+                <img src="images/PROMO.jpg" alt="">
+            </form>
+            <a class="close" href="#close">close</a>
+        </div>
+    </div>
+<!-- end popup -->
+</body>
+</html>
